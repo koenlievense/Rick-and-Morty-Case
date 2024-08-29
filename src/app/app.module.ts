718d142a7c7e20830158b2e9549ea2b5b0583629
koenlievense <60465482+koenlievense@users.noreply.github.com>
2024-08-29 19:40:37 +0200

    Update tsconfig. Add tabs to app component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { CharacterListComponent } from './components/character/list/character-list.component';
+import { LocationListComponent } from './components/location/list/location-list.component';
+import { EpisodeListComponent } from './components/episodes/list/episode-list.component';
+import { CharacterListItemComponent } from './components/character/list/list-item/character-list-item.component';
+import { LocationListItemComponent } from './components/location/list/list-item/location-list-item.component';
+import { EpisodeListItemComponent } from './components/episodes/list/list-item/episode-list-item.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    CharacterListComponent,
+    CharacterListItemComponent,
+    LocationListComponent,
+    LocationListItemComponent,
+    EpisodeListComponent,
+    EpisodeListItemComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
