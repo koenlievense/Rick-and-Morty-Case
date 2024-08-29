fe8fe6f2c1fe8be6bede5a41eefe3245dcfd6840
koenlievense <60465482+koenlievense@users.noreply.github.com>
2024-08-29 16:24:17 +0200

    Update karma config

--- karma.conf.js
+++ karma.conf.js
@@ -4,19 +4,16 @@ module.exports = function (config) {
     frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
       require("karma-jasmine"),
-      require("karma-chrome-launcher"), // Deze werkt ook voor Chromium
+      require("karma-chrome-launcher"),
       require("karma-jasmine-html-reporter"),
       require("karma-coverage"),
       require("@angular-devkit/build-angular/plugins/karma"),
     ],
     client: {
-      jasmine: {
-        // Hier kun je enkele Jasmine-opties instellen
-      },
-      clearContext: false, // Laat Jasmine Spec Runner de HTML niet wissen
+      clearContext: false,
     },
     jasmineHtmlReporter: {
-      suppressAll: true, // Verwijdert de logbalk
+      suppressAll: true,
     },
     coverageReporter: {
       dir: require("path").join(__dirname, "./coverage/your-angular-project"),
@@ -28,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["ChromiumHeadless"], // Gebruik 'ChromiumHeadless'
+    browsers: ["ChromiumHeadless"],
     singleRun: false,
     restartOnFileChange: true,
   });
