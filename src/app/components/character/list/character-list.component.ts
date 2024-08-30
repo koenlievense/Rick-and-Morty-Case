840aa9446a79fcac1ac7082190ad7305e4121038
koenlievense <60465482+koenlievense@users.noreply.github.com>
2024-08-30 14:08:09 +0200

    Add pagination component to character-list

--- src/app/components/character/list/character-list.component.ts
+++ src/app/components/character/list/character-list.component.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { CharacterService } from '../../../shared/services/character.service';
 import { Character } from '../../../shared/interfaces/character';
+import { HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-character-list',
   templateUrl: './character-list.component.html',
 })
 export class CharacterListComponent implements OnInit {
-  characters: Character[];
+  characters: Character[] = [];
+  currentPage: number = 1;
+  totalPages: number = 1;
 
   constructor(private characterService: CharacterService) {}
 
   ngOnInit(): void {
-    let params = '';
+    this.loadCharacters();
+  }
+
+  loadCharacters(): void {
+    let params = new HttpParams().set('page', this.currentPage.toString());
     this.characterService.fetchCharacters(params).subscribe((response) => {
       this.characters = response.results;
+      this.totalPages = response.info.pages;
     });
+  }
+
+  onPageChange(newPage: number): void {
+    this.currentPage = newPage;
+    this.loadCharacters();
   }
 }
