8fc099b5d1f34f08c2fe9dcd6d1479346bc8aa49
koenlievense <60465482+koenlievense@users.noreply.github.com>
2024-08-30 14:08:30 +0200

    Add pagination component to episode-list

--- src/app/components/episodes/list/episode-list.component.ts
+++ src/app/components/episodes/list/episode-list.component.ts
@@ -2,25 +2,37 @@ import { Component, Input, OnInit } from '@angular/core';
 import { EpisodeService } from '../../../shared/services/episode.service';
 import { Episode } from '../../../shared/interfaces/episode';
 import { Router } from '@angular/router';
+import { HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-episode-list',
   templateUrl: './episode-list.component.html',
 })
 export class EpisodeListComponent implements OnInit {
-  @Input()
-  episodes: Episode[];
+  episodes: Episode[] = [];
+  currentPage: number = 1;
+  totalPages: number = 1;
 
   constructor(private episodeService: EpisodeService, private router: Router) {}
 
   ngOnInit(): void {
-    let params = '';
+    this.loadEpisodes();
+  }
+
+  loadEpisodes(): void {
+    let params = new HttpParams().set('page', this.currentPage.toString());
     this.episodeService.fetchEpisodes(params).subscribe((response) => {
       this.episodes = response.results;
+      this.totalPages = response.info.pages;
     });
   }
 
-  navigate(id: number) {
+  onPageChange(newPage: number): void {
+    this.currentPage = newPage;
+    this.loadEpisodes();
+  }
+
+  navigate(id: number): void {
     this.router.navigate(['/episodes', id]);
   }
 }
