9584cdb0e4a6dba9f3a4834e172632f3af4655bd
koenlievense <60465482+koenlievense@users.noreply.github.com>
2024-08-30 14:08:46 +0200

    Add pagination component to location-list

--- src/app/components/location/list/location-list.component.ts
+++ src/app/components/location/list/location-list.component.ts
@@ -2,14 +2,16 @@ import { Component, Input, OnInit } from '@angular/core';
 import { LocationService } from '../../../shared/services/location.service';
 import { Location } from '../../../shared/interfaces/location';
 import { Router } from '@angular/router';
+import { HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-location-list',
   templateUrl: './location-list.component.html',
 })
 export class LocationListComponent implements OnInit {
-  @Input()
-  locations: Location[];
+  locations: Location[] = [];
+  currentPage: number = 1;
+  totalPages: number = 1;
 
   constructor(
     private locationService: LocationService,
@@ -17,13 +19,23 @@ export class LocationListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    let params = '';
+    this.loadLocations();
+  }
+
+  loadLocations(): void {
+    let params = new HttpParams().set('page', this.currentPage.toString());
     this.locationService.fetchLocations(params).subscribe((response) => {
       this.locations = response.results;
+      this.totalPages = response.info.pages;
     });
   }
 
-  navigate(id: number) {
-    this.router.navigate(['/episodes', id]);
+  onPageChange(newPage: number): void {
+    this.currentPage = newPage;
+    this.loadLocations();
+  }
+
+  navigate(id: number): void {
+    this.router.navigate(['/locations', id]);
   }
 }
